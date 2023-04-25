ba8e22302cc8690d1b638f53139ebf53ac4919a9
ReverseSerj <sej.petrov@gmail.com>
2023-04-25 10:53:37 +0300

    fix id

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -19,7 +19,7 @@ export default class PopupWithForm extends Popup {
     const popupInputs = this._arrayInputs;
     const popupInputValues = {};
     popupInputs.forEach((input) => {
-      popupInputValues[input.id] = input.value;
+      popupInputValues[input.name] = input.value;
     });
     return(popupInputValues);
   }
